5a053d73c31a820e28ca9a40297a165ce9a3fbc2
NLHawkins <nathanlhawkins@gmail.com>
2017-02-02 09:53:44 -0500

    started ef models and context

---
+++ NoteTaker/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoteTaker.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }        
+        public string Body { get; set; }
+        public string Timestamp { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
